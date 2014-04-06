d448a43a035586440fdd953a3f81fe111f9e10a9
Nick Bell <nick.j.bell@gmail.com>
2014-04-07 09:21:37 +1000

    Create Animal model
    Uses ruby-style lowercase variable names, which may need to be changed given the existing schema uses uppercase keys.

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,9 @@
 class Animal
   include NoBrainer::Document
+
+  field :colour, :type => String
+  field :gender, :type => String
+  field :name,   :type => String
+  field :notes,  :type => String
+  field :tag,     :type => Integer
 end
