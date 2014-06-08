e5acdd1dd7c29d51c9b9ff552526205717b173fd
Nick Bell <nick.j.bell@gmail.com>
2014-06-08 15:41:33 +1000

    Add didUpdateAddress event to AddressView
    View fires a didUpdateAddress with an address string as the
    argument whenever the street or suburb values are changed.

--- app/assets/javascripts/views/address_view.js
+++ app/assets/javascripts/views/address_view.js
@@ -1,14 +1,25 @@
 var App = window.App != null ? window.App : {};
 
 App.AddressView = Backbone.View.extend({
-  template: window.JST['location/address'],//$("#address-template").html()),
+  template: window.JST['location/address'],
+
+  events: {
+    'keyup #suburb' : 'updateAddress', 
+    'keyup #street' : 'updateAddress'
+  },
 
   initialize: function() {
-    _.bindAll(this, 'render');
+    _.bindAll(this, 'render', 'updateAddress');
   },
 
   render: function() {
     $(this.el).html(this.template());
     return this;
+  },
+
+  updateAddress: function() {
+    var address = this.$el.find('#street').val() + ', ' + this.$el.find('#suburb').val();
+    console.log('address: ', address);
+    this.trigger('didUpdateAddress', address);
   }
 });
--- spec/javascripts/views/AddressViewSpec.js
+++ spec/javascripts/views/AddressViewSpec.js
@@ -1,14 +1,25 @@
 describe("AddressView", function() {
 
   beforeEach(function() {
-    this.$view = new App.AddressView().render().$el;
+    this.view = new App.AddressView();
+    this.$el = this.view.render().$el;
   });
 
-  it("Renders a suburb field", function() {
-    expect(this.$view).toContainElement('#suburb');
+  it("should render a suburb field", function() {
+    expect(this.$el).toContainElement('#suburb');
   });
 
-  it("Renders a street field", function() {
-    expect(this.$view).toContainElement('#street');
+  it("should render a street field", function() {
+    expect(this.$el).toContainElement('#street');
+  });
+
+  it("should trigger a didUpdateAddress event on suburb or street entry", function() {
+    var spy = sinon.spy();
+    this.view.on('didUpdateAddress', spy);
+
+    this.$el.find('#suburb').val('Bondi').keyup();
+    this.$el.find('#street').val('Campbell Parade').keyup();
+
+    expect(spy.calledWith('Campbell Parade, Bondi')).toBeTruthy();
   });
 });
