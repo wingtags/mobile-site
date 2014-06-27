36e8e6c2c5ca2f3ca728e210e98c13ff51289f57
Nick Bell <nick.j.bell@gmail.com>
2014-06-27 10:41:36 +1000

    Add uploaded image preview
    Show a thumbnail of the captured image in a grid below the
    'Add photo' button.

--- app/assets/javascripts/templates/templates.js
+++ app/assets/javascripts/templates/templates.js
@@ -12,6 +12,11 @@ JST['image'] = _.template("\
       <input type='file' capture='camera' accept='image/*' id='camera-input' name='camera-input' style='display:none'> \
       <a href='#' id='camera-select' class='button expand'>Add Photo</a> \
     </div> \
+  </div> \
+  <div class='row'> \
+    <div class='large-6 columns'> \
+      <ul class='small-block-grid-2 medium-block-grid-3 large-block-grid-4'></ul> \
+    </div> \
   </div>");
 JST['thanks'] = _.template("<div class='row'> <div class='large-12 columns text-center'> <h2>You're awesome ;)</h2> <p>You've just found <strong><%= name %></strong>.</p> <p> <a href=''>Report another cockatoo</a><br /><a href='https://www.thankq.net.au/donate/v247/Donate1.aspx?ref=5617662807&pid=4'>Support us with a tax-deductible donation</a><br /><a href='https://www.facebook.com/CockatooWingtags'>Visit us on Facebook</a><br /> </p> </div> </div>");
 
--- app/assets/javascripts/views/image_view.js
+++ app/assets/javascripts/views/image_view.js
@@ -5,20 +5,52 @@ App.ImageView = Backbone.View.extend({
      'click #camera-select': 'getImage',
      'change #camera-input': 'fireDidUpdateImage'
    },
+
    template: window.JST['image'],
+
    initialize: function() {
-     _.bindAll(this, 'render', 'getImage');
+     _.bindAll(this, 
+      'render', 
+      'getImage', 
+      'renderImage',
+      'fireDidUpdateImage',
+      'appendImage');
    },
+
    render: function() {
      this.$el.html(this.template());
      return this;
    },
+
    getImage: function(event) {
      event.preventDefault();
      this.$el.find('#camera-input').click();
    },
+
    fireDidUpdateImage: function() {
     var file = this.$el.find('#camera-input').get(0).files[0];
+    this.renderImage(file);
     this.trigger('didUpdateImage', file);
-   }
+   },
+
+   renderImage: function(image) {
+    var reader = new FileReader();
+    var gallery = this.$el.find('ul');
+    var galleryItem = $("<li><a href='#' class='th'><img /></a></li>");
+
+    reader.readAsDataURL(image);
+    reader.onload = (function(item) { 
+      return function(e) { 
+        item.find('img').attr('src', e.target.result); 
+        
+      }; 
+    })(galleryItem);
+
+    gallery.append(galleryItem);
+   },
+
+   appendImage: function(url) { 
+    console.log('APPEND IMAGE');
+    this.$el.append("<img src=\"" + url + "\">");
+   },
  });
